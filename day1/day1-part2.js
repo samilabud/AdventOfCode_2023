225e9d23a93899b2e475765e28f26975d773c70b
Samil Abud <samilabud@gmail.com>
2023-12-03 00:34:18 -0400

    Finishing part 2 of day1

--- day1/day1-part2.js
+++ day1/day1-part2.js
@@ -1,15 +1,30 @@
 import fs from "fs";
-const textResult = fs.readFileSync("./day1/input.txt", "utf-8");
+let textResult = fs.readFileSync("./day1/input.txt", "utf-8");
+
+const letterToNumberObj = {
+  one: 1,
+  two: 2,
+  three: 3,
+  four: 4,
+  five: 5,
+  six: 6,
+  seven: 7,
+  eight: 8,
+  nine: 9,
+};
+
 const calibrations = textResult.split("\n");
 
-const firstDigitRegex = new RegExp(/\d/);
-const lastDigitRegex = new RegExp(/\d(?=\D*$)/);
+const letterExp = Object.keys(letterToNumberObj).join("|");
+const digitsRegex = new RegExp("\\d|" + letterExp, "g");
 
+const getDigit = (digit) => letterToNumberObj[digit] ?? Number(digit);
 const digits = calibrations.map((value) => {
   if (!value) return 0;
-  const firstDigit = value.match(firstDigitRegex);
-  const secondDigit = value.match(lastDigitRegex);
+  const matches = value.match(digitsRegex);
+  const firstDigit = getDigit(matches[0]);
+  const secondDigit = getDigit(matches[matches.length - 1]);
   return Number(String(firstDigit) + String(secondDigit));
 });
 const result = digits.reduce((accum, curr) => accum + curr, 0);
-console.log(`Day 1 - Result of part 1 => ${result}`);
+console.log(`Day 1 - Result of part 2 => ${result}`);
